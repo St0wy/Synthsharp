9cb6c238e962f2ffd58c1760a54a44c52becc743
ACKERMANNGUE <gawen.ackrm@eduge.ch>
2020-01-08 15:13:18 +0100

    All wave forms

--- Synthsharp/SynthView.Designer.cs
+++ Synthsharp/SynthView.Designer.cs
@@ -28,21 +28,338 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkOnOscillator1 = new System.Windows.Forms.CheckBox();
+            this.chkEnveloppeOscillator1 = new System.Windows.Forms.CheckBox();
+            this.chkVcfOscillator1 = new System.Windows.Forms.CheckBox();
+            this.chkLfoOscillator1 = new System.Windows.Forms.CheckBox();
+            this.chkVcaOscillator1 = new System.Windows.Forms.CheckBox();
+            this.chkVcoOscillator1 = new System.Windows.Forms.CheckBox();
+            this.cbxOscillator1 = new System.Windows.Forms.ComboBox();
+            this.groupbox2 = new System.Windows.Forms.GroupBox();
+            this.chkOnOscillator2 = new System.Windows.Forms.CheckBox();
+            this.chkEnveloppeOscillator2 = new System.Windows.Forms.CheckBox();
+            this.chkVcfOscillator2 = new System.Windows.Forms.CheckBox();
+            this.chkLfoOscillator2 = new System.Windows.Forms.CheckBox();
+            this.chkVcaOscillator2 = new System.Windows.Forms.CheckBox();
+            this.chkVcoOscillator2 = new System.Windows.Forms.CheckBox();
+            this.cbxOscillator2 = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chkOnOscillator3 = new System.Windows.Forms.CheckBox();
+            this.chkEnveloppeOscillator3 = new System.Windows.Forms.CheckBox();
+            this.chkVcfOscillator3 = new System.Windows.Forms.CheckBox();
+            this.chkLfoOscillator3 = new System.Windows.Forms.CheckBox();
+            this.chkVcaOscillator3 = new System.Windows.Forms.CheckBox();
+            this.chkVcoOscillator3 = new System.Windows.Forms.CheckBox();
+            this.cbxOscillator3 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupbox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkOnOscillator1);
+            this.groupBox1.Controls.Add(this.chkEnveloppeOscillator1);
+            this.groupBox1.Controls.Add(this.chkVcfOscillator1);
+            this.groupBox1.Controls.Add(this.chkLfoOscillator1);
+            this.groupBox1.Controls.Add(this.chkVcaOscillator1);
+            this.groupBox1.Controls.Add(this.chkVcoOscillator1);
+            this.groupBox1.Controls.Add(this.cbxOscillator1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(448, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oscillator 1";
+            // 
+            // chkOnOscillator1
+            // 
+            this.chkOnOscillator1.AutoSize = true;
+            this.chkOnOscillator1.Location = new System.Drawing.Point(386, 19);
+            this.chkOnOscillator1.Name = "chkOnOscillator1";
+            this.chkOnOscillator1.Size = new System.Drawing.Size(40, 17);
+            this.chkOnOscillator1.TabIndex = 6;
+            this.chkOnOscillator1.Text = "On";
+            this.chkOnOscillator1.UseVisualStyleBackColor = true;
+            // 
+            // chkEnveloppeOscillator1
+            // 
+            this.chkEnveloppeOscillator1.AutoSize = true;
+            this.chkEnveloppeOscillator1.Location = new System.Drawing.Point(300, 43);
+            this.chkEnveloppeOscillator1.Name = "chkEnveloppeOscillator1";
+            this.chkEnveloppeOscillator1.Size = new System.Drawing.Size(77, 17);
+            this.chkEnveloppeOscillator1.TabIndex = 5;
+            this.chkEnveloppeOscillator1.Text = "Enveloppe";
+            this.chkEnveloppeOscillator1.UseVisualStyleBackColor = true;
+            // 
+            // chkVcfOscillator1
+            // 
+            this.chkVcfOscillator1.AutoSize = true;
+            this.chkVcfOscillator1.Location = new System.Drawing.Point(300, 20);
+            this.chkVcfOscillator1.Name = "chkVcfOscillator1";
+            this.chkVcfOscillator1.Size = new System.Drawing.Size(46, 17);
+            this.chkVcfOscillator1.TabIndex = 4;
+            this.chkVcfOscillator1.Text = "VCF";
+            this.chkVcfOscillator1.UseVisualStyleBackColor = true;
+            // 
+            // chkLfoOscillator1
+            // 
+            this.chkLfoOscillator1.AutoSize = true;
+            this.chkLfoOscillator1.Location = new System.Drawing.Point(214, 66);
+            this.chkLfoOscillator1.Name = "chkLfoOscillator1";
+            this.chkLfoOscillator1.Size = new System.Drawing.Size(46, 17);
+            this.chkLfoOscillator1.TabIndex = 3;
+            this.chkLfoOscillator1.Text = "LFO";
+            this.chkLfoOscillator1.UseVisualStyleBackColor = true;
+            // 
+            // chkVcaOscillator1
+            // 
+            this.chkVcaOscillator1.AutoSize = true;
+            this.chkVcaOscillator1.Location = new System.Drawing.Point(214, 43);
+            this.chkVcaOscillator1.Name = "chkVcaOscillator1";
+            this.chkVcaOscillator1.Size = new System.Drawing.Size(47, 17);
+            this.chkVcaOscillator1.TabIndex = 2;
+            this.chkVcaOscillator1.Text = "VCA";
+            this.chkVcaOscillator1.UseVisualStyleBackColor = true;
+            // 
+            // chkVcoOscillator1
+            // 
+            this.chkVcoOscillator1.AutoSize = true;
+            this.chkVcoOscillator1.Location = new System.Drawing.Point(214, 20);
+            this.chkVcoOscillator1.Name = "chkVcoOscillator1";
+            this.chkVcoOscillator1.Size = new System.Drawing.Size(48, 17);
+            this.chkVcoOscillator1.TabIndex = 1;
+            this.chkVcoOscillator1.Text = "VCO";
+            this.chkVcoOscillator1.UseVisualStyleBackColor = true;
+            // 
+            // cbxOscillator1
+            // 
+            this.cbxOscillator1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxOscillator1.FormattingEnabled = true;
+            this.cbxOscillator1.Location = new System.Drawing.Point(6, 36);
+            this.cbxOscillator1.Name = "cbxOscillator1";
+            this.cbxOscillator1.Size = new System.Drawing.Size(121, 21);
+            this.cbxOscillator1.TabIndex = 0;
+           // this.cbxOscillator1.SelectedIndexChanged += new System.EventHandler(this.cbxOscillator1_SelectedIndexChanged);
+            // 
+            // groupbox2
+            // 
+            this.groupbox2.Controls.Add(this.chkOnOscillator2);
+            this.groupbox2.Controls.Add(this.chkEnveloppeOscillator2);
+            this.groupbox2.Controls.Add(this.chkVcfOscillator2);
+            this.groupbox2.Controls.Add(this.chkLfoOscillator2);
+            this.groupbox2.Controls.Add(this.chkVcaOscillator2);
+            this.groupbox2.Controls.Add(this.chkVcoOscillator2);
+            this.groupbox2.Controls.Add(this.cbxOscillator2);
+            this.groupbox2.Location = new System.Drawing.Point(31, 150);
+            this.groupbox2.Name = "groupbox2";
+            this.groupbox2.Size = new System.Drawing.Size(448, 100);
+            this.groupbox2.TabIndex = 7;
+            this.groupbox2.TabStop = false;
+            this.groupbox2.Text = "Oscillator 2";
+            // 
+            // chkOnOscillator2
+            // 
+            this.chkOnOscillator2.AutoSize = true;
+            this.chkOnOscillator2.Location = new System.Drawing.Point(386, 19);
+            this.chkOnOscillator2.Name = "chkOnOscillator2";
+            this.chkOnOscillator2.Size = new System.Drawing.Size(40, 17);
+            this.chkOnOscillator2.TabIndex = 6;
+            this.chkOnOscillator2.Text = "On";
+            this.chkOnOscillator2.UseVisualStyleBackColor = true;
+            // 
+            // chkEnveloppeOscillator2
+            // 
+            this.chkEnveloppeOscillator2.AutoSize = true;
+            this.chkEnveloppeOscillator2.Location = new System.Drawing.Point(300, 43);
+            this.chkEnveloppeOscillator2.Name = "chkEnveloppeOscillator2";
+            this.chkEnveloppeOscillator2.Size = new System.Drawing.Size(77, 17);
+            this.chkEnveloppeOscillator2.TabIndex = 5;
+            this.chkEnveloppeOscillator2.Text = "Enveloppe";
+            this.chkEnveloppeOscillator2.UseVisualStyleBackColor = true;
+            // 
+            // chkVcfOscillator2
+            // 
+            this.chkVcfOscillator2.AutoSize = true;
+            this.chkVcfOscillator2.Location = new System.Drawing.Point(300, 20);
+            this.chkVcfOscillator2.Name = "chkVcfOscillator2";
+            this.chkVcfOscillator2.Size = new System.Drawing.Size(46, 17);
+            this.chkVcfOscillator2.TabIndex = 4;
+            this.chkVcfOscillator2.Text = "VCF";
+            this.chkVcfOscillator2.UseVisualStyleBackColor = true;
+            // 
+            // chkLfoOscillator2
+            // 
+            this.chkLfoOscillator2.AutoSize = true;
+            this.chkLfoOscillator2.Location = new System.Drawing.Point(214, 66);
+            this.chkLfoOscillator2.Name = "chkLfoOscillator2";
+            this.chkLfoOscillator2.Size = new System.Drawing.Size(46, 17);
+            this.chkLfoOscillator2.TabIndex = 3;
+            this.chkLfoOscillator2.Text = "LFO";
+            this.chkLfoOscillator2.UseVisualStyleBackColor = true;
+            // 
+            // chkVcaOscillator2
+            // 
+            this.chkVcaOscillator2.AutoSize = true;
+            this.chkVcaOscillator2.Location = new System.Drawing.Point(214, 43);
+            this.chkVcaOscillator2.Name = "chkVcaOscillator2";
+            this.chkVcaOscillator2.Size = new System.Drawing.Size(47, 17);
+            this.chkVcaOscillator2.TabIndex = 2;
+            this.chkVcaOscillator2.Text = "VCA";
+            this.chkVcaOscillator2.UseVisualStyleBackColor = true;
+            // 
+            // chkVcoOscillator2
+            // 
+            this.chkVcoOscillator2.AutoSize = true;
+            this.chkVcoOscillator2.Location = new System.Drawing.Point(214, 20);
+            this.chkVcoOscillator2.Name = "chkVcoOscillator2";
+            this.chkVcoOscillator2.Size = new System.Drawing.Size(48, 17);
+            this.chkVcoOscillator2.TabIndex = 1;
+            this.chkVcoOscillator2.Text = "VCO";
+            this.chkVcoOscillator2.UseVisualStyleBackColor = true;
+            // 
+            // cbxOscillator2
+            // 
+            this.cbxOscillator2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxOscillator2.FormattingEnabled = true;
+            this.cbxOscillator2.Location = new System.Drawing.Point(6, 36);
+            this.cbxOscillator2.Name = "cbxOscillator2";
+            this.cbxOscillator2.Size = new System.Drawing.Size(121, 21);
+            this.cbxOscillator2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chkOnOscillator3);
+            this.groupBox3.Controls.Add(this.chkEnveloppeOscillator3);
+            this.groupBox3.Controls.Add(this.chkVcfOscillator3);
+            this.groupBox3.Controls.Add(this.chkLfoOscillator3);
+            this.groupBox3.Controls.Add(this.chkVcaOscillator3);
+            this.groupBox3.Controls.Add(this.chkVcoOscillator3);
+            this.groupBox3.Controls.Add(this.cbxOscillator3);
+            this.groupBox3.Location = new System.Drawing.Point(31, 269);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(448, 100);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Oscillator 3";
+            // 
+            // chkOnOscillator3
+            // 
+            this.chkOnOscillator3.AutoSize = true;
+            this.chkOnOscillator3.Location = new System.Drawing.Point(386, 19);
+            this.chkOnOscillator3.Name = "chkOnOscillator3";
+            this.chkOnOscillator3.Size = new System.Drawing.Size(40, 17);
+            this.chkOnOscillator3.TabIndex = 6;
+            this.chkOnOscillator3.Text = "On";
+            this.chkOnOscillator3.UseVisualStyleBackColor = true;
+            // 
+            // chkEnveloppeOscillator3
+            // 
+            this.chkEnveloppeOscillator3.AutoSize = true;
+            this.chkEnveloppeOscillator3.Location = new System.Drawing.Point(300, 43);
+            this.chkEnveloppeOscillator3.Name = "chkEnveloppeOscillator3";
+            this.chkEnveloppeOscillator3.Size = new System.Drawing.Size(77, 17);
+            this.chkEnveloppeOscillator3.TabIndex = 5;
+            this.chkEnveloppeOscillator3.Text = "Enveloppe";
+            this.chkEnveloppeOscillator3.UseVisualStyleBackColor = true;
+            // 
+            // chkVcfOscillator3
+            // 
+            this.chkVcfOscillator3.AutoSize = true;
+            this.chkVcfOscillator3.Location = new System.Drawing.Point(300, 20);
+            this.chkVcfOscillator3.Name = "chkVcfOscillator3";
+            this.chkVcfOscillator3.Size = new System.Drawing.Size(46, 17);
+            this.chkVcfOscillator3.TabIndex = 4;
+            this.chkVcfOscillator3.Text = "VCF";
+            this.chkVcfOscillator3.UseVisualStyleBackColor = true;
+            // 
+            // chkLfoOscillator3
+            // 
+            this.chkLfoOscillator3.AutoSize = true;
+            this.chkLfoOscillator3.Location = new System.Drawing.Point(214, 66);
+            this.chkLfoOscillator3.Name = "chkLfoOscillator3";
+            this.chkLfoOscillator3.Size = new System.Drawing.Size(46, 17);
+            this.chkLfoOscillator3.TabIndex = 3;
+            this.chkLfoOscillator3.Text = "LFO";
+            this.chkLfoOscillator3.UseVisualStyleBackColor = true;
+            // 
+            // chkVcaOscillator3
+            // 
+            this.chkVcaOscillator3.AutoSize = true;
+            this.chkVcaOscillator3.Location = new System.Drawing.Point(214, 43);
+            this.chkVcaOscillator3.Name = "chkVcaOscillator3";
+            this.chkVcaOscillator3.Size = new System.Drawing.Size(47, 17);
+            this.chkVcaOscillator3.TabIndex = 2;
+            this.chkVcaOscillator3.Text = "VCA";
+            this.chkVcaOscillator3.UseVisualStyleBackColor = true;
+            // 
+            // chkVcoOscillator3
+            // 
+            this.chkVcoOscillator3.AutoSize = true;
+            this.chkVcoOscillator3.Location = new System.Drawing.Point(214, 20);
+            this.chkVcoOscillator3.Name = "chkVcoOscillator3";
+            this.chkVcoOscillator3.Size = new System.Drawing.Size(48, 17);
+            this.chkVcoOscillator3.TabIndex = 1;
+            this.chkVcoOscillator3.Text = "VCO";
+            this.chkVcoOscillator3.UseVisualStyleBackColor = true;
+            // 
+            // cbxOscillator3
+            // 
+            this.cbxOscillator3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxOscillator3.FormattingEnabled = true;
+            this.cbxOscillator3.Location = new System.Drawing.Point(6, 36);
+            this.cbxOscillator3.Name = "cbxOscillator3";
+            this.cbxOscillator3.Size = new System.Drawing.Size(121, 21);
+            this.cbxOscillator3.TabIndex = 0;
             // 
             // SynthView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(520, 417);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupbox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "SynthView";
             this.Text = "Synthsharp";
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SynthView_KeyPress);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SynthView_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupbox2.ResumeLayout(false);
+            this.groupbox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkOnOscillator1;
+        private System.Windows.Forms.CheckBox chkEnveloppeOscillator1;
+        private System.Windows.Forms.CheckBox chkVcfOscillator1;
+        private System.Windows.Forms.CheckBox chkLfoOscillator1;
+        private System.Windows.Forms.CheckBox chkVcaOscillator1;
+        private System.Windows.Forms.CheckBox chkVcoOscillator1;
+        private System.Windows.Forms.ComboBox cbxOscillator1;
+        private System.Windows.Forms.GroupBox groupbox2;
+        private System.Windows.Forms.CheckBox chkOnOscillator2;
+        private System.Windows.Forms.CheckBox chkEnveloppeOscillator2;
+        private System.Windows.Forms.CheckBox chkVcfOscillator2;
+        private System.Windows.Forms.CheckBox chkLfoOscillator2;
+        private System.Windows.Forms.CheckBox chkVcaOscillator2;
+        private System.Windows.Forms.CheckBox chkVcoOscillator2;
+        private System.Windows.Forms.ComboBox cbxOscillator2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox chkOnOscillator3;
+        private System.Windows.Forms.CheckBox chkEnveloppeOscillator3;
+        private System.Windows.Forms.CheckBox chkVcfOscillator3;
+        private System.Windows.Forms.CheckBox chkLfoOscillator3;
+        private System.Windows.Forms.CheckBox chkVcaOscillator3;
+        private System.Windows.Forms.CheckBox chkVcoOscillator3;
+        private System.Windows.Forms.ComboBox cbxOscillator3;
     }
 }
 
--- Synthsharp/SynthView.cs
+++ Synthsharp/SynthView.cs
@@ -12,75 +12,26 @@ using System.Windows.Forms;
 
 namespace Synthsharp
 {
+
     public partial class SynthView : Form
     {
+        enum SelectedWave { Sine = 0, Square = 1, Triangle = 2, Sawtooth = 3, Noise = 4 };
         private const int SAMPLE_RATE = 44100;
         private const short BITS_PER_SAMPLE = 16;
         private const float DEFAULT_FREQUENCY = 440f;
+        private const float FREQUENCY_FOR_C2_NOTE = 65.4f;
+        private const float FREQUENCY_FOR_C3_NOTE = 130.8f;
+        private const float FREQUENCY_FOR_C4_NOTE = 261.6f;
+        private const float FREQUENCY_FOR_C5_NOTE = 523.2f;
+        private const float FREQUENCY_FOR_C6_NOTE = 1046.5f;
+        private const float FREQUENCY_FOR_C7_NOTE = 2093f;
+        private const float FREQUENCY_FOR_C8_NOTE = 4186f;
+        private readonly string[] DEFAULT_COMBO_BOX = { "Sine", "Square", "Saw", "Triangle", "Noise" };
+
         public SynthView()
         {
             InitializeComponent();
-        }
-
-        private void SynthView_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            short[] wave = new short[SAMPLE_RATE];
-            float frequency = DEFAULT_FREQUENCY;
-            /* Algorithms are available at this address : https://blogs.msdn.microsoft.com/dawate/2009/06/25/intro-to-audio-programming-part-4-algorithms-for-different-sound-waves-in-c/ */
-
-            /* 
-            short.MaxValue is the Amplitude 
-            ((Math.PI * 2 * frequency) / SAMPLE_RATE) is the t (angular frequency)
-            i is itself (time unit) 
-            */
-
-            /* Sine wave */
-            for (int i = 0; i < SAMPLE_RATE; i++)
-            {
-                /* 
-                 Equation used 
-                 Sample = Amplitude * sin(t*i)
-                */
-
-                wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i));
-            }
-            /* Square wave */
-            for (int i = 0; i < SAMPLE_RATE; i++)
-            {
-                /* 
-                Equation used
-                Sample = Amplitude * sgn(sin(t * i)) 
-                */
-                wave[i] = Convert.ToInt16(short.MaxValue * Math.Sign(Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i)));
-            }
-
-            short tmpSample = -short.MaxValue;
-            int samplesPerWaveLength = (int)(SAMPLE_RATE / frequency);
-            short ampStep = (short)((short.MaxValue * 2) / samplesPerWaveLength);
-
-            /* Triangle wave */
-            for (int i = 0; i < SAMPLE_RATE; i++)
-            {
-                /* Used to reset the slope */
-                if (Math.Abs(tmpSample + ampStep) > short.MaxValue)
-                {
-                    ampStep = (short)-ampStep;
-                }
-                tmpSample += ampStep;
-                wave[i] = tmpSample;
-            }
-            /* Sawtooth wave */
-            for (int i = 0; i < SAMPLE_RATE; i++)
-            {
-                for (int j = 0; j < samplesPerWaveLength && i < SAMPLE_RATE; j++)
-                {
-                    tmpSample += ampStep;
-                    wave[i++] = Convert.ToInt16(tmpSample);
-                }
-                i--;
-            }
-
-            new SoundPlayer(createWave(wave)).Play();
+            InitializeComboBox(new ComboBox[] { cbxOscillator1, cbxOscillator2, cbxOscillator3 }, DEFAULT_COMBO_BOX);
         }
         /// <summary>
         /// Create a sound in wave format
@@ -117,6 +68,135 @@ namespace Synthsharp
             binaryWriter.Write(binaryWave);
             memoryStream.Position = 0;
             return memoryStream;
+        }
+
+        /// <summary>
+        /// Initialize the combobox values
+        /// </summary>
+        /// <param name="cbs">Array of ComboBox</param>
+        /// <param name="dataCbx">Elements of the ComboBox</param>
+        public void InitializeComboBox(ComboBox[] cbs, string[] dataCbx)
+        {
+            foreach (ComboBox combobox in cbs)
+            {
+                combobox.Items.AddRange(dataCbx);
+            }
+        }
+
+        //private void cbxOscillator1_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    int index = cbxOscillator1.SelectedIndex;
+        //    switch (index)
+        //    {
+        //        case (int)SelectedWave.Sine:
+
+        //            break;
+        //        case (int)SelectedWave.Square:
+
+        //            break;
+        //        case (int)SelectedWave.Sawtooth:
+
+        //            break;
+        //        case (int)SelectedWave.Triangle:
+
+        //            break;
+        //        case (int)SelectedWave.Noise:
+
+        //            break;
+        //        default:
+        //            break;
+
+        //    }
+        //}
+
+        private void SynthView_KeyDown(object sender, KeyEventArgs e)
+        {
+            short[] wave = new short[SAMPLE_RATE];
+            float frequency = DEFAULT_FREQUENCY;
+
+            Random rdm = new Random();
+
+            short tmpSample = -short.MaxValue;
+            int samplesPerWaveLength = (int)(SAMPLE_RATE / frequency);
+            short ampStep = (short)((short.MaxValue * 2) / samplesPerWaveLength);
+
+            /* Frenquencies available at this address : https://en.wikipedia.org/wiki/Piano_key_frequencies (using C2 to C8)*/
+            switch (e.KeyCode)
+            {
+                case Keys.Y:
+                    frequency = FREQUENCY_FOR_C2_NOTE;
+                    break;
+                case Keys.X:
+                    frequency = FREQUENCY_FOR_C3_NOTE;
+                    break;
+                case Keys.C:
+                    frequency = FREQUENCY_FOR_C4_NOTE;
+                    break;
+                case Keys.V:
+                    frequency = FREQUENCY_FOR_C5_NOTE;
+                    break;
+                case Keys.B:
+                    frequency = FREQUENCY_FOR_C6_NOTE;
+                    break;
+                case Keys.N:
+                    frequency = FREQUENCY_FOR_C7_NOTE;
+                    break;
+                case Keys.M:
+                    frequency = FREQUENCY_FOR_C8_NOTE;
+                    break;
+                default:
+
+                    break;
+            }
+
+
+            /* Algorithms are available at this address : https://blogs.msdn.microsoft.com/dawate/2009/06/25/intro-to-audio-programming-part-4-algorithms-for-different-sound-waves-in-c/ */
+
+            /* 
+            short.MaxValue is the Amplitude 
+            ((Math.PI * 2 * frequency) / SAMPLE_RATE) is the t (angular frequency)
+            i is itself (time unit) 
+            */
+
+            /* Sine wave */
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i));
+            }
+            /* Square wave */
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                wave[i] = Convert.ToInt16(short.MaxValue * Math.Sign(Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i)));
+            }
+
+            /* Triangle wave */
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                /* Used to reset the slope */
+                if (Math.Abs(tmpSample + ampStep) > short.MaxValue)
+                {
+                    ampStep = (short)-ampStep;
+                }
+                tmpSample += ampStep;
+                wave[i] = tmpSample;
+            }
+            /* Sawtooth wave */
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                for (int j = 0; j < samplesPerWaveLength && i < SAMPLE_RATE; j++)
+                {
+                    tmpSample += ampStep;
+                    wave[i++] = Convert.ToInt16(tmpSample);
+                }
+                i--;
+            }
+            /* Noise wave */
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                wave[i] = (short)rdm.Next(-short.MaxValue, short.MaxValue);
+            }
+
+            new SoundPlayer(createWave(wave)).Play();
         }
     }
 }
